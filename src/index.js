815d3d78153c33ca9f65fff48379c169fa5c90b0
Ozias M. Junior <oziasjunior@live.com>
2022-07-17 20:56:53 -0300

    Implementação de validação da função
    Implementado a validação se na string contem apenas números, e se a quantidade de caracteres está correta.
    Adicionado as mensagens de erro.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-function DocumentBrazil(number) {
-    let result = false;
-    if (number.length === 11) {
-        number = number.split("")
+function DocumentBrazil(documentNumber) {
+    if (!/^[0-9]+$/.test(number))
+        return { isValid: false, value: documentNumber, name: "DocumentException", message: "The value entered must contain only numbers." }
+
+    if (documentNumber.length !== 11)
+        return { isValid: false, value: documentNumber, name: "DocumentException", message: "The value informed must be numeric with 11 digits." }
+
+    if (documentNumber.length === 11) {
+        let number = documentNumber.split("")
         let VerifyingDigit = []
         for (let digit = 0; VerifyingDigit.length < 2; digit++) {
             let calculate = 0;
@@ -10,10 +15,15 @@ function DocumentBrazil(number) {
             }
             VerifyingDigit[digit] = calculate % 11 < 2 ? 0 : 11 - (calculate % 11)
         }
-        result = VerifyingDigit[0] === parseInt(number[9]) && VerifyingDigit[1] === parseInt(number[10])
+        let result = VerifyingDigit[0] === parseInt(number[9]) && VerifyingDigit[1] === parseInt(number[10]);
+
+        return {
+            isValid: result,
+            value: documentNumber,
+            name: result ? "DocumentSuccess" : "DocumentException",
+            message: result ? "Document informed is valid." : "The document informed is not valid check if it was typed correctly."
+        }
     }
-    console.log(result);
-    return result
 }
 
 export default DocumentBrazil
